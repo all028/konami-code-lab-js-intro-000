440d33a01532e4dd3b061b3ab81c70134aa8459f
al <albliu@email.com>
2019-12-07 00:19:22 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,13 +17,23 @@ function init() {
     const key = event.key;
     var detectFlag = 0
 
-    if (key === "ArrowUp") {
+    if (key === codes[index]) {
+      index++
+      if(key === codes[index]){
+        index++
+        if (key === codes[index]){
+          
+        }else{
+          
+        }        
+      }else{
+        detectFlag = 2
+      }//2nd
+    else{
       detectFlag = 1
-    else {
-      if (key === "ArrowUp") {
-          detectFlag = 1
-      }
-    }
+    } //1st if
+
+    
 
 
 

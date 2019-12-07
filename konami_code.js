94dceefc49803b42658c12ac2bb933ac73cccc22
al <albliu@email.com>
2019-12-07 03:37:33 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -15,14 +15,14 @@ function init() {
   document.body.addEventListener('keydown', function(event) {
 
     const key = event.key;
-  let index = 0
+
 
 
       if (key === codes[index]) {
         index++;
 
         if (index === codes.length) {
-        //  alert('congratulatory')
+          alert('congratulatory')
           index = 0;
 
         }
